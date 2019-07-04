cb3c3419fe82ee9c0acbff13dd332faa7474ec85
kyotarooooo <i.kentaro-0916@outlook.com>
2019-07-04 21:42:20 +0900

    ユーザー側作成（エラーなし）

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def top
+  	@user = current_user
+  end
+
+  def index
+  	@users = User.all
+  end
+
+
+  def show
+  	@user = User.find(params[:id])
+  	@coordinates = Coordinate.where(user_id: @user)
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	redirect_to user_path(@user.id)
+  end
+
+
+  private
+  def user_params
+  	params.require(:user).permit(:profile_image, :introduction, :kanji_name, :kana_name, :user_name, :sex, :height, :email)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-	root "homes#about"
-  devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
+
+root "homes#about"
+get "users/:id/top/" => "users#top", as: "mypage"
+
+
+
+devise_for :admins, controllers: {
+  	sessions:      'admins/sessions',
+  	passwords:     'admins/passwords',
+  	registrations: 'admins/registrations'
 }
 devise_for :users, controllers: {
-  sessions:      'users/sessions',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations'
+	sessions:      'users/sessions',
+  	passwords:     'users/passwords',
+  	registrations: 'users/registrations'
 }
-
 resources :coordinates, only:[:new, :create, :index, :show, :edit, :update, :destroy]
+resources :users, only:[:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
